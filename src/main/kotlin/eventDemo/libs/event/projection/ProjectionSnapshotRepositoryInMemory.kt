dee0d32d28466bc755d9b1103384b46bb8d85fb9
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2025-03-23 00:08:28 +0100

    Move SnapshotConfig class

--- src/main/kotlin/eventDemo/libs/event/projection/ProjectionSnapshotRepositoryInMemory.kt
+++ src/main/kotlin/eventDemo/libs/event/projection/ProjectionSnapshotRepositoryInMemory.kt
@@ -10,27 +10,6 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.minutes
-
-data class SnapshotConfig(
-  /**
-   * Keep snapshot when is on the head of the queue cache
-   */
-  val maxSnapshotCacheSize: Int = 20,
-  /**
-   * Keep snapshot when is newer of
-   *
-   *     snapshot.date > now + maxSnapshotCacheTtl
-   */
-  val maxSnapshotCacheTtl: Duration = 10.minutes,
-  /**
-   * Keep snapshot when version is this modulo
-   *
-   *     snapshot.lastVersion % modulo == 1
-   */
-  val modulo: Int = 10,
-)
 
 class ProjectionSnapshotRepositoryInMemory<E : Event<ID>, P : Projection<ID>, ID : AggregateId>(
   private val eventStore: EventStore<E, ID>,
---
+++ src/main/kotlin/eventDemo/libs/event/projection/SnapshotConfig.kt
@@ -0,0 +1,23 @@
+package eventDemo.libs.event.projection
+
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
+data class SnapshotConfig(
+  /**
+   * Keep snapshot when is on the head of the queue cache
+   */
+  val maxSnapshotCacheSize: Int = 20,
+  /**
+   * Keep snapshot when is newer of
+   *
+   *     snapshot.date > now + maxSnapshotCacheTtl
+   */
+  val maxSnapshotCacheTtl: Duration = 10.minutes,
+  /**
+   * Keep snapshot when version is this modulo
+   *
+   *     snapshot.lastVersion % modulo == 1
+   */
+  val modulo: Int = 10,
+)
